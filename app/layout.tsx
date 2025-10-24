0d4b5dc8e3389c7ed692e8f7af9ae8322d38320a
wahaab1122 <abdulwahaab@wyb.ac.lk>
2025-10-24 15:10:13 +0530

    Alaxee files

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
         {/* <FullscreenWrapper> */}
           <div className="relative flex flex-col h-screen">
             <Navbar />
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-           <LibraryBig className="text-primary" />
+          <LibraryBig className="text-primary" />
             <p className="font-bold text-primary text-2xl">Alaxeebook</p>
           </NextLink>
         </NavbarBrand>
